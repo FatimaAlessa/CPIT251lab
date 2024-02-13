f7e1ac1987c0e8edc084cb11011aa850ccf9ddfa
ffaat <falialessa@stu.kau.edu.sa>
2024-02-13 02:43:05 -0800

    hey

---
+++ src/main/java/com/mycompany/cpit251/CPIT251.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cpit251;
+
+/**
+ *
+ * @author ffaat
+ */
+public class CPIT251 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
